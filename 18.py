cad4cd4b61776378cfb1f9beb7cafd40e1404f65
EdgarGev94 <edgar.gevorgyan.1994@gmail.com>
2021-03-09 21:40:52 +0400

    lesson18

---
+++ 18.py
@@ -0,0 +1,49 @@
+# students = {"Ani" : 10 , 
+# 			"Aram" : 9 , 
+# 			"Vahag" : 10 , 
+# 			"Elen" : 9 , 
+# 			"Davit" : 6 ,
+# 			"Tigran" : 7 ,
+# 			"Zara" : 8 ,
+# 			"Rafo" : 2 , 
+# 			"Mari" : 4 ,
+# 			"Anna" : 7
+# 			}
+# lav = max(students.values())
+# vat = min(students.values())
+# gerazanc = {}
+# cuylik = {}
+# for i in students:
+# 	if students[i] == lav:
+# 		gerazanc[i] = lav
+# 	if students[i] == vat:
+# 		cuylik[i] = vat
+# print("gerazanc" , gerazanc)
+# print("cuylik" , cuylik)
+# m = 0
+# for i in students.values():
+# 	m += i 
+# mean = m / len(students)
+# print(mean)
+
+
+
+
+# s = "a,2,b,5,c,8,a,4,e,11".split(",")
+
+# c = dict()
+# for i in range(0 , len(s) - 1 , 2):
+# 	if s[i] in c:
+# 		c[s[i]] = int(c[s[i]]) + int(s[i + 1])
+# 	else:
+# 		c[s[i]] = s[i + 1]
+# print(c)
+
+
+
+# text = "araaaa"
+# c = {}
+# for i in text:
+# 	count = text.count(i)
+# 	c[i] = count
+# print(c)
