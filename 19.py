c0387c307d0a9ca1dfb272c841d17db833441743
EdgarGev94 <edgar.gevorgyan.1994@gmail.com>
2021-03-09 21:35:00 +0400

    lesson1

---
+++ 19.py
@@ -0,0 +1,10 @@
+# a = {"1" , 1 , 5 , 1 , "hi" , "a"}
+# #a.remove(254)
+# a.discard(0)
+# print(a)
+
+# set1 = {12 , 23 , "11" , "hello"}
+# set2 = {14 , 3 , "141" , 54}
+# print(set1.isdisjoint(set2))
+
+
